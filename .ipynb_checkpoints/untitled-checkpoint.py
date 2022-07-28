a8b298e386140807c61fcde05e8e1073e7f5c02c
cjkim_law <cjkim_law@gimchanjun-ui-MacBookPro.local>
2022-07-28 11:16:25 +0900

    test lint

---
+++ .ipynb_checkpoints/untitled-checkpoint.py
@@ -0,0 +1,18 @@
+def    hello_word(test) :
+    print("hello {}".format(test))
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__" :
+    hello_word("cj")
