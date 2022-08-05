e07da4f097dc9d5cf4e4eb9dcd0544cea4ee0f20
Chanjun-kim <nice3803@gmail.com>
2022-08-05 01:27:07 +0000

    test

--- hello_world_test.py
+++ hello_world_test.py
@@ -1,16 +1,15 @@
-def hello_world() :
+'''
+작성자 : CJ
+git hook을 사용한 Pylint & flake8 테스트용 모듈
+'''
+
+
+def hello_world():
+    '''
+    pylint & flake8 테스트용 함수
+    '''
     print("hello world")
-    
-    return
 
 
-
-
-
-
-
-
-
-
-if   __name__ == "__main__" :
+if __name__ == "__main__":
     hello_world()
